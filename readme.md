ee1343d9d6710106c29a8ea6122374b3b5bc7a6d
amey-kudari <work.ameykudari@gmail.com>
2021-05-13 22:44:31 +0530

    finish flask app and docs, and add a readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+# BTP PROJECT
+## MYDB DATABASE WITH FLASK ENDPOINTS
+
+### HOSTING THE DATABASE
+Note: python 3 and python3-pip needs to be installed on the system
+```
+pip install flask; 
+python server.js;
+```
+
+~ Amey and Bhavesh
--- server.py
+++ server.py
@@ -1,10 +1,61 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
-
+from flask import Flask, render_template, url_for, request
+import json, os
 app = Flask(__name__)
 
+
+"""
+DATABASE
+"""
+class database():
+    def __init__(self, name):
+        self.name = './databases/' + name+'.mydb'
+        if not os.path.exists(self.name):
+            open(self.name, 'w')
+    def insert(self, rrow):
+        row = '|,|'.join([str(i) for i in rrow])
+        open(self.name, 'a').write(row + '\n')
+    def ret(self):
+        return json.dumps([i.split('|,|') for i in open(self.name,'r').read().strip().split('\n')])
+    def clear(self):
+        open(self.name, 'w')
+"""
+END DATABASE
+"""
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/docs')
+def docs():
+    return render_template('docs.html')
+
+# requests.post(url+'insert/qwerasdfzxcv', data = {'row' : json.dumps([1,2,'asdf'])}).content
+@app.route("/query/<tablename>", methods = ['POST','GET', 'DELETE'])
+def insert(tablename):
+    print('GOT')
+    if request.method == 'POST' and len(tablename.strip()) > 0:
+        print(tablename.strip())
+        row = json.loads(request.form['row'])
+        print("POSTTT!!!!")
+        db = database(tablename.strip())
+        db.insert(row)
+        return 'INSERTED'
+    elif request.method == 'GET' and len(tablename.strip()) > 0:
+        db = database(tablename.strip())
+        return db.ret()
+    elif request.method == 'DELETE' and len(tablename.strip()) > 0:
+        db = database(tablename.strip())
+        db.clear()
+        return 'DELETED'
+    else:
+        return 'MAKE A POST REQUEST WITH VALID TABLENAME'
+
+
+# @app.route('/get')
+# def ret():
+
 
 if __name__ == '__main__':
     app.run(debug=True)
